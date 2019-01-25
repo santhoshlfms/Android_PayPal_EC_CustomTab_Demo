f73f819dda4dc47d39b66c0d4b782e45dde1ab14
Nelson <sannelson@paypal.com>
2019-01-25 11:27:56 +0530

    logic fix

--- app/src/main/java/com/example/paypalcustomtabdemo/MainActivity.java
+++ app/src/main/java/com/example/paypalcustomtabdemo/MainActivity.java
@@ -18,11 +18,15 @@ import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.paypal.android.lib.riskcomponent.RiskComponent;
+import com.paypal.android.lib.riskcomponent.SourceApp;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import cz.msebera.android.httpclient.entity.StringEntity;
@@ -30,7 +34,7 @@ import cz.msebera.android.httpclient.entity.mime.Header;
 
 public class MainActivity extends AppCompatActivity {
     private final int CHROME_CUSTOM_TAB_REQUEST_CODE = 100;
-
+    Map<String, Object> additionalParams = new HashMap<String, Object>();
 
     public void showLoader(Boolean setVisibilty) {
         ProgressBar progress = (ProgressBar) findViewById(R.id.progressBar);
@@ -59,17 +63,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button btnCart = (Button) findViewById(R.id.cartbutton);
+        // valid for paypal only
+        final String  paringId = RiskComponent.getInstance().init(this.getApplicationContext(), SourceApp.UNKNOWN, "",additionalParams);
+        String againParingId = RiskComponent.getInstance().init(this.getApplicationContext(), paringId, SourceApp.UNKNOWN, "",additionalParams);
+        Log.d("riskparingId :",paringId);
+        Log.d("againParingId :",againParingId);
+        // we might use this for other payments
+        String riskParingId = RiskComponent.getInstance().getPairingID();
+        //after getting risk paring id
+        Log.d("body :", riskParingId);
+        String riskPayload = RiskComponent.getInstance().getRiskPayload().toString();
+        //debug
+        Log.d("dsadsadsa",RiskComponent.getInstance().getRiskPayload().names().toString());
+        Log.d("riskPayload" , riskPayload);
+
+        System.setProperty("dyson.debug.mode", Boolean.TRUE.toString());
+
         btnCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
+                String url = "https://www.hannam18014850.qa.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=EC-142650723S898581C";
+                String packageName = "com.android.chrome";
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                CustomTabsIntent customTabsIntent = builder.build();
+                // check if chrome is installed if installed always open in chrome
+                // so we can have OneTouch Feature !
+                if(isPackageInstalled(packageName)) {
+                    customTabsIntent.intent.setPackage(packageName);
+                }
+                customTabsIntent.intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                Log.d("URL" , url);
+                customTabsIntent.intent.setData(Uri.parse(url));
+                startActivityForResult(customTabsIntent.intent, CHROME_CUSTOM_TAB_REQUEST_CODE);
+
+                /*try{
                     JSONObject productJson = new JSONObject();
                     productJson.put("description","This is camera");
                     productJson.put("shipping","5");
                     productJson.put("tax","2");
                     productJson.put("shipping_discount","-3");
                     productJson.put("total","17");
-                    productJson.put("currency","USD");
+                    productJson.put("currency","INR");
                     productJson.put("intent","sale");
                     productJson.put("subtotal","10.00");
                     productJson.put("name","Camera");
@@ -78,8 +113,13 @@ public class MainActivity extends AppCompatActivity {
                     productJson.put("handling_fee","1");
                     productJson.put("insurance","2");
                     productJson.put("customFlag","false");
+                    productJson.put("return_url","com.example.paypalcustomtabdemo");
+                    productJson.put("cancel_url","https://node-paypal-express-sever.herokuapp.com/error.html");
+
                     showLoader(true); // set loader visible
 
+                    // send the risk id
+                    productJson.put("riskParingId",paringId);
                     // make api call here ....
 
                     AsyncHttpClient client = new AsyncHttpClient();
@@ -89,12 +129,14 @@ public class MainActivity extends AppCompatActivity {
                     InputStream inputStream = assetManager.open("app.properties");
                     properties.load(inputStream);
                     String url = properties.get("createPayments").toString();
+                    Log.d("TAG", "onSuccess: ********************* onCancel");
                     client.post(getApplicationContext(), url, requestData, "application/json", new JsonHttpResponseHandler(){
                         @Override
                         public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
                             // code block to open up URL in Custom Tab
                             try{
                                 String url = Utility.getUrlFromJSONArray(response);
+                                Log.d("TAG", "onSuccess: ");
                                 if(!url.equals(null)) {
                                     showLoader(false);
                                     String packageName = "com.android.chrome";
@@ -107,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                     customTabsIntent.intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                                     customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    Log.d("URL" , url);
                                     customTabsIntent.intent.setData(Uri.parse(url));
                                     startActivityForResult(customTabsIntent.intent, CHROME_CUSTOM_TAB_REQUEST_CODE);
                                 }
@@ -121,7 +164,7 @@ public class MainActivity extends AppCompatActivity {
                     });
                 }catch (Exception e) {
                     e.printStackTrace();
-                }
+                }*/
             }
         });
     }
--- app/src/main/java/com/example/paypalcustomtabdemo/ProcessComplete.java
+++ app/src/main/java/com/example/paypalcustomtabdemo/ProcessComplete.java
@@ -25,8 +25,10 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.InputStream;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 import cz.msebera.android.httpclient.entity.StringEntity;
 import cz.msebera.android.httpclient.entity.mime.Header;
@@ -41,48 +43,138 @@ public class ProcessComplete extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.process_complete);
+        Intent i = getIntent();
+        Bundle bundle = i.getExtras();
+        if (bundle != null) {
+            for (String key : bundle.keySet()) {
+                Object value = bundle.get(key);
+                Log.d("LOG_TAG", String.format("%s %s (%s)", key,
+                        value.toString(), value.getClass().getName()));
+            }
+        }
+        //System.out.println("INT DATA : " + i.getStringExtra("PayerID"));
 
-        List<String> params = getIntent().getData().getPathSegments();
-        TextView textView = (TextView) findViewById(R.id.status);
-        final TextView textLoader = (TextView) findViewById(R.id.statusLoading);
-        final HighlightJsView highlightJsView = (HighlightJsView) findViewById(R.id.highlight_view);
-        if(params.get(0).equals("success")){
-            textView.setText("Transaction Sucesss");
+        System.out.println("URI : " + i.getData());
+        Uri dataFromPayPal = i.getData();
+        if(dataFromPayPal  != null) {
+            final  String  paymentId = dataFromPayPal.getQueryParameter("paymentId");
+            final  String  payerId = dataFromPayPal.getQueryParameter("PayerID");
+
+            System.out.println("paymentId : " + paymentId);
+            System.out.println("PayerID : " + payerId);
+
+
             try{
-
                 AsyncHttpClient client = new AsyncHttpClient();
                 client.setTimeout(60000);
-                Properties properties = new Properties();
+                final Properties properties = new Properties();
                 AssetManager assetManager = getApplicationContext().getAssets();
                 InputStream inputStream = assetManager.open("app.properties");
                 properties.load(inputStream);
                 String url = properties.get("getPaymentDetails").toString();
-                url = url+"?token="+params.get(1)+"&payerID="+params.get(2);
+                url = url + "?token=" + paymentId + "&payerID=" + "31343242";
                 JSONObject obj = new JSONObject();
                 StringEntity requestData = new StringEntity(obj.toString());
 
-                client.post( url,  new JsonHttpResponseHandler(){
+                client.post(url, new JsonHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
                         // pass the result to an sdk
-                        highlightJsView.setTheme(Theme.ANDROID_STUDIO);
-                        highlightJsView.setHighlightLanguage(Language.AUTO_DETECT);
-                        try{
-                            highlightJsView.setSource(String.valueOf(String.valueOf(response.toString(4))));
-                            textLoader.setVisibility(View.INVISIBLE);
-                        }catch(JSONException e){
+                        try {
+                            JSONObject payer = response.getJSONObject("payer");
+                            JSONObject payer_info = payer.getJSONObject("payer_info");
+                            String payerId = payer_info.getString("payer_id");
+                            System.out.println("payerId : " + payerId);
+
+                            String executeUrl = properties.get("executePayment").toString();
+                            executeUrl = executeUrl + "?paymentId=" + paymentId + "&PayerID=" + payerId;
+                            System.out.println("executeUrl : " + executeUrl);
+                            AsyncHttpClient client = new AsyncHttpClient();
+                            client.get(executeUrl, new JsonHttpResponseHandler() {
+                                @Override
+                                public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                                    // pass the result to an sdk
+                                    final TextView textLoader = (TextView) findViewById(R.id.statusLoading);
+                                    final HighlightJsView highlightJsView = (HighlightJsView) findViewById(R.id.highlight_view);
+                                    highlightJsView.setTheme(Theme.ANDROID_STUDIO);
+                                    highlightJsView.setHighlightLanguage(Language.AUTO_DETECT);
+                                    try {
+                                        highlightJsView.setSource(String.valueOf(String.valueOf(response.toString(4))));
+                                        textLoader.setVisibility(View.INVISIBLE);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
                 });
-
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-
-        }else{
-            textView.setText("Transaction Failed/Pending");
-
         }
-    }
+
+
+
+
+        /*Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();*/
+        Log.d("URI","*******************************************************");
+       /* System.out.println("URI : " + appLinkIntent.getData());
+        System.out.println("id : " + appLinkData.getQueryParameter("id"));
+        if(appLinkData != null) {
+            String token = appLinkData.getQueryParameter("id");
+
+            setContentView(R.layout.process_complete);
+*/
+            // Log.d("status", status);Log.d("payerId", payerId);Log.d("token", token);
+           /* TextView textView = (TextView) findViewById(R.id.status);
+            final TextView textLoader = (TextView) findViewById(R.id.statusLoading);
+            final HighlightJsView highlightJsView = (HighlightJsView) findViewById(R.id.highlight_view);*/
+         //   if (token.length() > 0) {
+               // textView.setText("Transaction Sucesss :" + token);
+            /*    try {
+
+                    AsyncHttpClient client = new AsyncHttpClient();
+                    client.setTimeout(60000);
+                    Properties properties = new Properties();
+                    AssetManager assetManager = getApplicationContext().getAssets();
+                    InputStream inputStream = assetManager.open("app.properties");
+                    properties.load(inputStream);
+                    String url = properties.get("getPaymentDetails").toString();
+                    url = url + "?token=" + token + "&payerID=" + payerId;
+                    JSONObject obj = new JSONObject();
+                    StringEntity requestData = new StringEntity(obj.toString());
+
+                    client.post(url, new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                            // pass the result to an sdk
+                            highlightJsView.setTheme(Theme.ANDROID_STUDIO);
+                            highlightJsView.setHighlightLanguage(Language.AUTO_DETECT);
+                            try {
+                                highlightJsView.setSource(String.valueOf(String.valueOf(response.toString(4))));
+                                textLoader.setVisibility(View.INVISIBLE);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }*/
+
+          /*  } else {
+                textView.setText("Transaction Failed/Pending");
+
+            }*/
+        }
+        // ATTENTION: This was auto-generated to handle app links.
+
+  //  }
 }
